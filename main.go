81404d277c310f12feb1d08edcd00d46b5de593b
Aryan Ahmed <thearyanahmed@gmail.com>
2025-02-26 15:04:31 +0000

    Update main.go

--- main.go
+++ main.go
@@ -17,124 +17,18 @@ func logRequest(r *http.Request) {
 	method := r.Method
 	fmt.Println("Got request!", method, uri)
 }
-
-type User struct {
-	ID   int
-	Name string
-	Age  int
-}
-
-type DBConfig struct {
-	User     string
-	Password string
-	Host     string
-	Port     int
-	DBName   string
-}
-
 func main() {
 
-	// Open a connection to the MySQL database
-	// Load the database configuration from environment variables
-	config := DBConfig{
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Host:     os.Getenv("DB_HOST"),
-		Port:     25060, // Replace with your port
-		DBName:   os.Getenv("DB_NAME"),
-	}
-
-	connStr := fmt.Sprintf("%s://%s:%s@%s:%d/%s", "mysql", config.User, config.Password, config.Host, config.Port, config.DBName)
-	// Build the MySQL connection string using the struct
-	fmt.Println(connStr)
-	// db, err := sql.Open("mysql", connStr)
-	// if err != nil {
-	//     log.Fatal("Error opening database connection: ", err)
-	// }
-	// defer db.Close()
-
-	// // Check if the connection is successful by pinging the database
-	// err = db.Ping()
-	// if err != nil {
-	//     log.Fatal("Error pinging database: ", err)
-	// }
-
-	// http.HandleFunc("/setup", func(w http.ResponseWriter, r *http.Request) {
-	//     logRequest(r)
-
-	//     // Create the users table (if not already created)
-	//     createTableSQL := `CREATE TABLE IF NOT EXISTS users (
-	//         id INT AUTO_INCREMENT PRIMARY KEY,
-	//         name VARCHAR(100),
-	//         age INT
-	//     );`
-	//     _, err = db.Exec(createTableSQL)
-	//     if err != nil {
-	//         fmt.Fprintf(w, "something went wrong %v", err.Error())
-	//     }
-
-	//     fmt.Fprintf(w, "done")
-	// })
-
-	// http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-	//     logRequest(r)
-
-	//     // Retrieve and display the inserted values
-	//     rows, err := db.Query("SELECT id, name, age FROM users")
-	//     if err != nil {
-	//         log.Fatal("Error retrieving data: ", err)
-	//     }
-	//     defer rows.Close()
-
-	//     var users []User
-	//     for rows.Next() {
-	//         var user User
-	//         if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
-	//             log.Fatal("Error scanning row: ", err)
-	//         }
-	//         users = append(users, user)
-	//     }
-
-	//     // Check for any row iteration errors
-	//     if err := rows.Err(); err != nil {
-	//         log.Fatal("Error during rows iteration: ", err)
-	//     }
-
-	//     // Print the retrieved users
-	//     fmt.Println("Retrieved Users:")
-	//     for _, user := range users {
-	//         fmt.Printf("ID: %d, Name: %s, Age: %d\n", user.ID, user.Name, user.Age)
-	//     }
-
-	//     fmt.Fprint(w, "\nusers get query")
-	// })
-
-	// http.HandleFunc("/seed/users", func(w http.ResponseWriter, r *http.Request) {
-	// 	logRequest(r)
-
-	// 	// Insert 10 random values into the users table
-	// 	insertSQL := "INSERT INTO users (name, age) VALUES (?, ?)"
-	// 	for i := 0; i < 10; i++ {
-	// 		name := fmt.Sprintf("User%d", rand.Intn(1000))
-	// 		age := rand.Intn(60) + 18 // Random age between 18 and 77
-	// 		_, err := db.Exec(insertSQL, name, age)
-	// 		if err != nil {
-	// 			log.Fatal("Error inserting data: ", err)
-	// 		}
-	// 	}
-
-	// 	fmt.Fprint(w, "requestID.String()")
-	// })
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
-	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8001"
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
+	})
+
 
 	for _, encodedRoute := range strings.Split(os.Getenv("ROUTES"), ",") {
 		if encodedRoute == "" {
