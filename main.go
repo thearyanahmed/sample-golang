17902777175ba125f7a86dc8176cd2844ab8e923
Aryan <paryan@digitalocean.com>
2025-03-17 11:12:45 +0000

    Add header stuff

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-        "sync"
-         "time"
 	"net/http"
 	"os"
 	"strings"
@@ -16,15 +13,20 @@ func logRequest(r *http.Request) {
 	fmt.Println("Got request!", method, uri)
 }
 func main() {
-
-
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8001"
+		port = "8080"
 	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		logRequest(r)
 		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
+
+        for name, headers := range r.Header {
+            for _, h := range headers {
+                fmt.Fprintf(w, " %v = %v\n", name, h)
+            }
+        }
 	})
 
 
@@ -43,37 +45,8 @@ func main() {
 	fmt.Println()
 	fmt.Printf("==> Server listening at %s 🚀\n", bindAddr)
 
-	       go graduallyIncreaseLoad(1*time.Minute, 5*time.Second, 10)
-
 	if err := http.ListenAndServe(bindAddr, nil); err != nil {
 		panic(err)
 	}
 }
 
-
-// graduallyIncreaseLoad increases CPU load by spawning more goroutines over time.
-func graduallyIncreaseLoad(duration time.Duration, increment time.Duration, goroutinesPerIncrement int) {
-        startTime := time.Now()
-        var wg sync.WaitGroup
-
-        for time.Since(startTime) < duration {
-                for i := 0; i < goroutinesPerIncrement; i++ {
-                        wg.Add(1)
-                        go func() {
-                                defer wg.Done()
-                                for {
-                                        // Simple CPU-intensive loop.
-                                        _ = 1 + 1
-                                        // Allow other goroutines to run.
-                                        runtime.Gosched()
-                                }
-                        }()
-                }
-                time.Sleep(increment)
-        }
-
-        // Wait for all goroutines to finish (though they won't, in this example).
-        // This is mainly to prevent the program from exiting before the load is applied.
-        time.Sleep(duration) // Keep running for the desired duration
-        //wg.Wait() //Uncomment to wait, but the go routines will never finish.
-}
